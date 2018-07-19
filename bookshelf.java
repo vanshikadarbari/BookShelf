0f4701ef0684631744bafadbadc3805e1abc43cb
vanshikadarbari <vanshikadarbari@yahoo.com>
2018-07-19 16:41:37 +0530

    added checking existing file status.

--- bookshelf.java
+++ bookshelf.java
@@ -1,10 +1,5 @@
 import java.io.*;
-<<<<<<< HEAD
 
-=======
-import java.util.ArrayList;
-import java.util.Scanner;
->>>>>>> 6b57148d81a8d5c9f1df753558ca93a7496fd182
 
 class book
 {
@@ -34,7 +29,7 @@ class Bookshelf
     public static void main(String[] args)
             throws IOException
     {
-<<<<<<< HEAD
+
         BufferedReader sc=new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter number of users.");
         int n=Integer.parseInt(sc.readLine());
@@ -42,26 +37,29 @@ class Bookshelf
         {
             System.out.println("What is your name?");
             String name=sc.readLine();
-=======
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter number of users.");
-        int n=sc.nextInt();
-        for(int i=1;i<=n;i++)
-        {
-            System.out.println("What is your name?");
-            String name=sc.next();
->>>>>>> 6b57148d81a8d5c9f1df753558ca93a7496fd182
+            name=name.toLowerCase();
             String fname=name+".txt";
-            PrintWriter writer = new PrintWriter(fname, "UTF-8");
+            PrintWriter writer=null;
+            FileWriter fw=null;
+            //BufferedWriter bw=null;
+            boolean filexists;
+            filexists = new File(fname).exists();
+
+            if(!filexists)
+            writer = new PrintWriter(fname, "UTF-8");
+            else
+            {
+                fw=new FileWriter(fname,true);
+                //bw = new BufferedWriter(fw);
+            }
 
             System.out.println("How many books do you want to input?");
-<<<<<<< HEAD
             int nob=Integer.parseInt(sc.readLine());
             for(int j=0;j<nob;j++)
             {
                 System.out.println("Enter the title of the book.");
                 String title=sc.readLine();
-               // sc.nextLine();
+
 
                 System.out.println("Enter the author of the book.");
                 String author=sc.readLine();
@@ -71,33 +69,33 @@ class Bookshelf
 
                 System.out.println("How much would you rate it on a scale from 1-5, with 1 being the lowest?");
                 int rate=Integer.parseInt(sc.readLine());
-=======
-            int nob=sc.nextInt();
-            sc.nextLine();
-            for(int j=0;j<nob;j++)
-            {
-                System.out.println("Enter the title of the book.");
-                String title=sc.next();
 
-                System.out.println("Enter the author of the book.");
-                String author=sc.next();
-
-                System.out.println("Enter the genre.");
-                String genre=sc.next();
-
-                System.out.println("How much would you rate it on a scale from 1-5, with 1 being the lowest?");
-                int rate=sc.nextInt();
->>>>>>> 6b57148d81a8d5c9f1df753558ca93a7496fd182
 
                 book b=new book(title,author,genre,rate);
-                writer.println("Title: "+b.title);
-                writer.println("Written by: "+b.author);
-                writer.println("Genre: "+b.genre);
-                writer.println("Rating: "+b.rating);
-                writer.print("\n\n\n");
+                if(!filexists)
+                {
+                    writer.println("Title: "+b.title);
+                    writer.println("Written by: "+b.author);
+                    writer.println("Genre: "+b.genre);
+                    writer.println("Rating: "+b.rating);
+                    writer.print("\n\n\n");
+                    writer.flush();
+                }
+                else
+                {
+                    fw.write("Title: "+b.title+"\n");
+                    fw.write("Written by: "+b.author+"\n");
+                    fw.write("Genre: "+b.genre+"\n");
+                    fw.write("Rating: "+b.rating+"\n");
+                    fw.write("\n\n\n");
+                }
 
             }
+            if(writer!=null)
             writer.close();
+            if(fw!=null)
+            fw.close();
+
 
 
         }
